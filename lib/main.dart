b119d52be949736fc83ad811883ec2daf29abc8c
Your Name <you@example.com>
2022-02-23 17:31:48 +0900

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:new_gradient_app_bar/new_gradient_app_bar.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
-  runApp(MyApp());
+  initializeDateFormatting().then((_) => runApp(MainPage()));
+}
+class MainPage extends StatefulWidget{
+  MyApp createState()=> MyApp();
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends State<MainPage> {
+  DateTime _selectedDay = DateTime.now();
+  DateTime _focusedDay = DateTime.now();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "MoonApp",
       theme: ThemeData.light(),
       home: Scaffold(
-        appBar: NewGradientAppBar(
-          gradient: LinearGradient(
-            colors: [Colors.lightBlue.shade200, Colors.deepPurple.shade200],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
-        ),
         body: Container(
-          
+          child: Column(
+            children: [
+              Center(
+                child: Container(
+                  child: TableCalendar(
+                    firstDay: DateTime.utc(1900, 10, 16),
+                    lastDay: DateTime.utc(2030, 3, 14),
+                    focusedDay: _focusedDay,
+                    selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+                    onDaySelected: (selectedDay, focusedDay) {
+                      setState(() {
+                        _selectedDay = selectedDay;
+                      });
+                    },
+                  )
+                ),
+              ),
+            ]
+          ),
         ),
       ),
     );
